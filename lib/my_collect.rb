f5097c777e9419cd3aff989ffa803d7bfe68e8f8
Jacob  Lam <jacob.lam.work@gmail.com>
2019-03-18 23:11:54 +0000

    Automatically backed up by Learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,4 +3,5 @@ def my_collect(original_array)
   collected_array = []
   while i < original_array.length 
   i += 1 
-  yield 
+  yield collected_array <<  ( original_array.split("").first )
+  end 
